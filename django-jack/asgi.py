d4b0ccc3ae26bd63c26c49f971faa94bb0025a14
matsu <50922910+mtsml@users.noreply.github.com>
2020-08-27 23:17:13 +0900

    django_tutorialからdjango-jackへプロジェクト名を変更

--- django-jack/asgi.py
+++ django-jack/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for django_tutorial project.
+ASGI config for django-jack project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_tutorial.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django-jack.settings')
 
 application = get_asgi_application()
