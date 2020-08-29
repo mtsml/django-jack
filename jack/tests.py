92e4f1cbd66a6f016cae102a1b5e6ee469bbb5bd
matsu <50922910+mtsml@users.noreply.github.com>
2020-08-30 03:04:14 +0900

    indexのテストを追加

--- jack/tests.py
+++ jack/tests.py
@@ -1,3 +1,48 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Channel
+
+
+class IndexViewTests(TestCase):
+    def test_no_channel(self):
+        """
+        テーブルにチャンネルが存在しない場合は、メッセージが表示される
+        """
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'No Recomended Youtuber.')
+        self.assertQuerysetEqual(response.context['channel_list'], [])
+
+    def test_display_channel(self):
+        """
+        テーブルにチャンネルが存在する場合は、チャンネルの一覧が表示される
+        """
+        channel_id = 'TokaiOnAir'
+        channel = Channel.objects.create(channel_id=channel_id, channel_nm='東海オンエア')
+        response = self.client.get(reverse('index'))
+        self.assertQuerysetEqual(
+            response.context['channel_list'], 
+            [f'<Channel: {channel_id}>']
+        )
+
+    def test_add_channel(self):
+        """
+        チャンネルを追加すると、テーブルにオブジェクトが追加され、リダイレクトされる
+        """
+        channel_id = 'TokaiOnAir'
+        response = self.client.post('/', {'channel_id': channel_id, 'channel_nm': '東海オンエア'})
+        self.assertQuerysetEqual(
+            Channel.objects.all(),
+            [f'<Channel: {channel_id}>']
+        )
+        self.assertEqual(response.status_code, 302)
+
+    def test_add_invalid_channel(self):
+        """
+        追加しようとしたチャンネルが実在しない場合、テーブルにオブジェクトは作成されず、エラーメッセージが表示されれる
+        """
+        channel_id = 'TokaiOnAirJanai'
+        response = self.client.post('/', {'channel_id': channel_id, 'channel_nm': '東海オンエアじゃない'})
+        self.assertContains(response, 'そのチャンネルIDは存在しません')
+        self.assertQuerysetEqual(response.context['channel_list'], [])
