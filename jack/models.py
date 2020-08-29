c5b7f944035825f12cc12972c483c34a8fbe68ab
matsu <50922910+mtsml@users.noreply.github.com>
2020-08-30 02:31:44 +0900

    可視性を高めるためオブジェクトにchannel_idを表示させる

--- jack/models.py
+++ jack/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Channel(models.Model):
     channel_id = models.CharField(max_length=200) # TODO:チャンネルIDの最大文字数を調べる
     channel_nm = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.channel_id
