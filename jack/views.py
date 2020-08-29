532cef7561d4995d0c3ed104acf8b45d9d4d7e09
matsu <50922910+mtsml@users.noreply.github.com>
2020-08-30 00:14:18 +0900

    バリデーションを追加

--- jack/views.py
+++ jack/views.py
@@ -6,13 +6,15 @@ from .models import Channel
 from .forms import ChannelForm
 
 def index(request):
+    message=None # TODO:messageの処理はもっといい方法があるはず
     if request.method == 'POST':
         form = ChannelForm(request.POST)
         if form.is_valid():
             form.save()
-        return HttpResponseRedirect(reverse('index'))
-    else:
-        form = ChannelForm()
+            return HttpResponseRedirect(reverse('index'))
+        else:
+            message = 'そのチャンネルIDは存在しません'
+    form = ChannelForm()
     channel_list = Channel.objects.all()
-    context = {'form': form, 'channel_list': channel_list}    
+    context = {'form': form, 'channel_list': channel_list, 'message': message}    
     return render(request, 'jack/index.html', context)
