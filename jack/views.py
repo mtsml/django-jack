233cf153a7779687da2361a8897f1a3d0951b4b6
matsu <50922910+mtsml@users.noreply.github.com>
2020-08-29 22:57:02 +0900

    チャンネル登録機能の追加

--- jack/views.py
+++ jack/views.py
@@ -1,4 +1,6 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 from .models import Channel
 
@@ -7,3 +9,11 @@ def index(request):
     channel_list = Channel.objects.all()
     context = {'channel_list': channel_list}
     return render(request, 'jack/index.html', context)
+
+
+def add_channel(request):
+    channel_id = request.POST['channel_id']
+    channel_nm = request.POST['channel_nm']
+    channel = Channel(channel_id=channel_id, channel_nm=channel_nm)
+    channel.save()
+    return HttpResponseRedirect(reverse('index'))
